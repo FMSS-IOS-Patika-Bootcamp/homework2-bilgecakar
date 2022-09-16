ca8c1eff75dd5795d085bd84c9d929046421f996
Bilge Çakar <36795459+bilgecakar@users.noreply.github.com>
2022-09-16 18:14:16 +0300

    Added Navigation Controller

--- NewsApp/NewsApp/HomePageViewController.swift
+++ NewsApp/NewsApp/HomePageViewController.swift
@@ -7,13 +7,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomePageViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //Customize navigation title
+        self.navigationController?.navigationBar.customNavigationBar()
+        
     }
 
-
 }
-
